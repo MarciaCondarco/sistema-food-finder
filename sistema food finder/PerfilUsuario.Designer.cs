d0ea0e2152f7f4b41a4353aded129825e33eb2e3
Marcia Condarco <marciagisseli21@gmail.com>
2025-02-04 11:50:07 -0300

    adicionando a janela perfil do usuario

---
+++ sistema food finder/PerfilUsuario.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace sistema_food_finder
+{
+    partial class PerfilUsuario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PerfilUsuario));
+            this.maskedTextBoxCPF = new System.Windows.Forms.MaskedTextBox();
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.textBoxSenha = new System.Windows.Forms.TextBox();
+            this.textBoxNomeCompleto = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelCadastrarUsuario = new System.Windows.Forms.Label();
+            this.buttonCadastrar = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // maskedTextBoxCPF
+            // 
+            this.maskedTextBoxCPF.Location = new System.Drawing.Point(219, 347);
+            this.maskedTextBoxCPF.Mask = "000,000,000-00";
+            this.maskedTextBoxCPF.Name = "maskedTextBoxCPF";
+            this.maskedTextBoxCPF.Size = new System.Drawing.Size(384, 20);
+            this.maskedTextBoxCPF.TabIndex = 30;
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.Location = new System.Drawing.Point(268, 400);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(75, 23);
+            this.buttonLogin.TabIndex = 28;
+            this.buttonLogin.Text = "Voltar";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            // 
+            // textBoxEmail
+            // 
+            this.textBoxEmail.Location = new System.Drawing.Point(219, 298);
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(384, 20);
+            this.textBoxEmail.TabIndex = 26;
+            // 
+            // textBoxSenha
+            // 
+            this.textBoxSenha.Location = new System.Drawing.Point(219, 253);
+            this.textBoxSenha.Name = "textBoxSenha";
+            this.textBoxSenha.Size = new System.Drawing.Size(384, 20);
+            this.textBoxSenha.TabIndex = 24;
+            // 
+            // textBoxNomeCompleto
+            // 
+            this.textBoxNomeCompleto.Location = new System.Drawing.Point(219, 205);
+            this.textBoxNomeCompleto.Name = "textBoxNomeCompleto";
+            this.textBoxNomeCompleto.Size = new System.Drawing.Size(384, 20);
+            this.textBoxNomeCompleto.TabIndex = 23;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(216, 331);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(27, 13);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "CPF";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(216, 282);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 13);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "Email";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(216, 237);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Senha";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(216, 189);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Nome Completo";
+            // 
+            // labelCadastrarUsuario
+            // 
+            this.labelCadastrarUsuario.AutoSize = true;
+            this.labelCadastrarUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCadastrarUsuario.Location = new System.Drawing.Point(296, 27);
+            this.labelCadastrarUsuario.Name = "labelCadastrarUsuario";
+            this.labelCadastrarUsuario.Size = new System.Drawing.Size(191, 33);
+            this.labelCadastrarUsuario.TabIndex = 16;
+            this.labelCadastrarUsuario.Text = "Perfil Usuario";
+            this.labelCadastrarUsuario.Click += new System.EventHandler(this.labelCadastrarUsuario_Click);
+            // 
+            // buttonCadastrar
+            // 
+            this.buttonCadastrar.Location = new System.Drawing.Point(429, 400);
+            this.buttonCadastrar.Name = "buttonCadastrar";
+            this.buttonCadastrar.Size = new System.Drawing.Size(75, 23);
+            this.buttonCadastrar.TabIndex = 29;
+            this.buttonCadastrar.Text = "Salvar";
+            this.buttonCadastrar.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(313, 63);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(156, 107);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 31;
+            this.pictureBox1.TabStop = false;
+            // 
+            // PerfilUsuario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.maskedTextBoxCPF);
+            this.Controls.Add(this.buttonCadastrar);
+            this.Controls.Add(this.buttonLogin);
+            this.Controls.Add(this.textBoxEmail);
+            this.Controls.Add(this.textBoxSenha);
+            this.Controls.Add(this.textBoxNomeCompleto);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelCadastrarUsuario);
+            this.Name = "PerfilUsuario";
+            this.Text = "PerfilUsuario";
+            this.Load += new System.EventHandler(this.PerfilUsuario_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MaskedTextBox maskedTextBoxCPF;
+        private System.Windows.Forms.Button buttonLogin;
+        private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.TextBox textBoxSenha;
+        private System.Windows.Forms.TextBox textBoxNomeCompleto;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelCadastrarUsuario;
+        private System.Windows.Forms.Button buttonCadastrar;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
